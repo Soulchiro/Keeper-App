c729ef51938847b245718f99726594a8cb4a0fe0
Vladislove <sunseeker93@mail.ru>
2024-01-16 20:58:48 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaKiwiBird } from "react-icons/fa";
+
+function Header() {
+  return (
+    <header>
+      <h1><FaKiwiBird />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
